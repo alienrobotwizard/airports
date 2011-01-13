2e2dd80c7298527f9ac70c1f641680b516b451ca
Jacob Perkins <jacob.a.perkins@gmail.com>
2011-01-12 21:32:38 -0600

    incorporate passengers per seat as the metric for bubble color

--- www/utils/tsv_to_jsvar.rb
+++ www/utils/tsv_to_jsvar.rb
@@ -7,8 +7,11 @@ require 'json'
 
 class Mapper < Wukong::Streamer::RecordStreamer
   def process *args
-    airport_code, lat, lng, passenger_degree, flights_degree, year = args
-    yield [year, airport_code, lat, lng, passenger_degree, flights_degree, passenger_degree.to_f / flights_degree.to_f]
+    airport_code, lat, lng, passenger_degree, seats_degree, flights_degree, year = args
+    ppf = passenger_degree.to_f / flights_degree.to_f
+    pps = ((passenger_degree.to_f / seats_degree.to_f)*100.0).to_i/100.0 rescue 0.0
+    spf = seats_degree.to_f / flights_degree.to_f
+    yield [year, airport_code, lat, lng, passenger_degree, flights_degree, ppf, pps, spf]
   end
 end
 
@@ -25,8 +28,8 @@ class Reducer < Wukong::Streamer::AccumulatingReducer
 
   # year, lat, lon, passenger_degree, flights_degree, airport_code
   def accumulate *args
-    year, airport_code, lat, lng, passenger_degree, flights_degree, passengers_per_flight = args
-    @var[year.to_i] << {:lat => lat.to_f, :lng => lng.to_f, :passenger_degree => passenger_degree.to_i, :flights_degree => flights_degree.to_i, :airport_code => airport_code, :ppf => passengers_per_flight.to_f.round}
+    year, airport_code, lat, lng, passenger_degree, flights_degree, ppf, pps, spf = args
+    @var[year.to_i] << {:lat => lat.to_f, :lng => lng.to_f, :passenger_degree => passenger_degree.to_i, :flights_degree => flights_degree.to_i, :airport_code => airport_code, :ppf => ppf.to_f.round, :pps => pps.to_f, :spf => spf.to_f}
   end
 
   def finalize &blk
