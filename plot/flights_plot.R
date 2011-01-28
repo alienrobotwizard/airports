e987c0546fcd04007674064689a0c47748ed1974
Jacob Perkins <jacob.a.perkins@gmail.com>
2011-01-28 09:24:56 -0600

    finalization of r code

--- plot/flights_plot.R
+++ plot/flights_plot.R
@@ -2,8 +2,8 @@
 library(ggplot2);
 
 # Read data in and take a subset
-degrees        <- read.table('yearly_degrees.tsv', header=FALSE, sep='\t', colClasses=c('character', 'character', 'numeric', 'numeric'));
-names(degrees) <- c('airport_code', 'year', 'passenger_degree', 'flights_degree');
+degrees        <- read.table('data/yearly_degrees.tsv', header=FALSE, sep='\t', colClasses=c('character', 'character', 'numeric', 'numeric', 'numeric'));
+names(degrees) <- c('airport_code', 'year', 'passenger_degree', 'seats_degree', 'flights_degree');
 select_degrees <- subset(degrees, year=='2000' | year=='2001' | year=='2002');
 
 # Plotting with ggplot2
--- plot/make_plots.sh
+++ plot/make_plots.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
-if [ -e 'yearly_degrees.tsv' ]
+if [ -e 'data/yearly_degrees.tsv' ]
 then 
 R --vanilla < flights_plot.R
 R --vanilla < passenger_plot.R
 else
-  echo -e "Can't find \"yearly_degrees.tsv\"\n\nPlease calculate the yearly degree distribution and place in this directory."
+  echo -e "Can't find \"yearly_degrees.tsv\"\n\nPlease calculate the yearly degree distribution and place into data/yearly_degrees.tsv"
 fi
